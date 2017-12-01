76b5421b513f759f282572022e67734b8a2fc30c
visha12l <vsvshinde15@gmail.com>
2017-12-01 21:06:34 +0530

    filter code

--- JS_App/js/app.js
+++ JS_App/js/app.js
@@ -1,33 +1,48 @@
 function addName() {
   let userInput = document.getElementById('userInput').value;
+  let data = [];
+  data.push(userInput);
+  let parentNode = document.getElementById('studentList');
   if(!_.isEmpty(userInput)) {
-    let parentWrap = document.createElement('li');
-    parentWrap.setAttribute('class', 'clearfix list-group-item list-group-item-success');
-    let textContent = document.createElement('span');
-    textContent.setAttribute('class', 'pull-left studentName')
-    let deleteButton = document.createElement('button');
-    deleteButton.setAttribute('class', 'pull-right btn btn-danger')
-    deleteButton.textContent = 'Delete'
-    textContent.innerHTML = userInput;
-    document.getElementById('studentList').appendChild(parentWrap);
-    parentWrap.appendChild(textContent);
-    parentWrap.appendChild(deleteButton);
-    document.getElementById('userInput').value = '';
-    deleteButton.addEventListener('click', function (e) {
-      e.currentTarget.parentElement.classList.add('hide');
+    let node = _.map(data, (index, key) => {
+        return `<li>
+                    <span>hi</span>
+                    <button>Delete</button>
+                </li>`;
     });
-  } else {
-    alert('enter student name');
+    console.log(node);
+    //parentNode.appendChild(node);
+  //   let parentWrap = document.createElement('li');
+  //   parentWrap.setAttribute('class', 'clearfix list-group-item list-group-item-success');
+  //   let textContent = document.createElement('span');
+  //   textContent.setAttribute('class', 'pull-left studentName')
+  //   let deleteButton = document.createElement('button');
+  //   deleteButton.setAttribute('class', 'pull-right btn btn-danger')
+  //   deleteButton.textContent = 'Delete'
+  //   textContent.innerHTML = userInput;
+  //   document.getElementById('studentList').appendChild(parentWrap);
+  //   parentWrap.appendChild(textContent);
+  //   parentWrap.appendChild(deleteButton);
+  //   document.getElementById('userInput').value = '';
+  //   deleteButton.addEventListener('click', function (e) {
+  //     e.currentTarget.parentElement.classList.add('hide');
+  //   });
+  // } else {
+  //   alert('enter student name');
+  // }
   }
 }
-
-function searchName() {
-    let searchTerm = event.currentTarget.value;
-    let list = document.getElementsByClassName('studentName');
-    let result = _.filter(list, (index, key) => {
-        return index.innerHTML.indexOf(searchTerm) > -1 ;
-    });
-    let data  = _.map(result, (index, key) => {
-        index.innerHTML;
-    });
-}
+//
+// function searchName() {
+//     let searchTerm = event.currentTarget.value;
+//     let list = document.getElementsByClassName('studentName');
+//     let result = _.filter(list, (index, key) => {
+//         return index.innerHTML.indexOf(searchTerm) > -1 ;
+//     });
+//     let data  = _.map(result, (index, key) => {
+//         return index.innerHTML;
+//     });
+//     let searchResult = _.map(data, (value, key) => {
+//         return `<span>${value}</span>`;
+//     });
+// }
